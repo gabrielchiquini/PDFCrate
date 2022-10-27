2cf66c9e674ce80bbaa0aaf4a6f3b90fd9d488e4
Gabriel Chiquini <gabrielchiquini@hotmail.com>
2022-10-27 10:26:09 -0300

    Adding a custom font on CustomTextStyle example

--- examples/src/main/java/pdfcrate/examples/CustomTextStyle.java
+++ examples/src/main/java/pdfcrate/examples/CustomTextStyle.java
@@ -6,18 +6,20 @@ import pdfcrate.document.Document;
 import pdfcrate.util.Edges;
 
 import java.awt.*;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
 
 import static pdfcrate.examples.Common.LOREM_IPSUM_PARAGRAPH;
 
 /** Modify the default text style */
 public class CustomTextStyle {
-  public static void main(String[] args) throws FileNotFoundException {
+  public static void main(String[] args) throws IOException {
     final var document = new Document();
+    final var customFont = document.loadFont(Objects.requireNonNull(fontInputStream()));
     document
-        // using one of the 14 default fonts, to use a custom font
-        // add it to the document with loadFont()
+        // using one of the 14 default fonts
         .textStyle(document.textStyle().toBuilder().fontSize(20f).font(PDType1Font.COURIER).build())
         .margin(Edges.all(20))
         // will use the text style defined above
@@ -27,11 +29,22 @@ public class CustomTextStyle {
             new Paragraph(
                 LOREM_IPSUM_PARAGRAPH,
                 document.textStyle().toBuilder()
-                    .font(PDType1Font.TIMES_BOLD_ITALIC)
+                    // using a custom font
+                    .font(customFont)
                     // line spacing, relative to font size
                     .leading(1.5f)
                     .textColor(new Color(0x234F1E))
                     .build()))
         .render(new FileOutputStream("test-output/custom-text-style.pdf"));
+  }
+
+  // loading the font file from resources directory
+  public static InputStream fontInputStream() throws IOException {
+    ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+    final var url = classloader.getResource("Roboto-Regular.ttf");
+    if (url == null) {
+      return null;
+    }
+    return url.openStream();
   }
 }
--- examples/src/main/java/pdfcrate/examples/GenerateAll.java
+++ examples/src/main/java/pdfcrate/examples/GenerateAll.java
@@ -1,11 +1,11 @@
 package pdfcrate.examples;
 
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class GenerateAll {
-  public static void main(String[] args) throws FileNotFoundException, URISyntaxException {
+  public static void main(String[] args) throws IOException, URISyntaxException {
     final var outputDir = new File("test-output");
     if (!outputDir.exists()) {
       //noinspection ResultOfMethodCallIgnored
